663c6c51507c020d1b6c5f947ee14aeecc748d3e
Jamila <jamnjeri@gmail.com>
2023-01-11 13:03:10 +0300

    Added a comment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './views/Home';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
       <Home />
-      <Footer />
     </div>
   );
 }
